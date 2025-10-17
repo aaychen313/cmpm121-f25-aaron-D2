f74f41e5267a5ab817cd57c03c7b5c4e2fefda52
aaychen313 <aaron111305@gmail.com>
2025-10-17 22:05:12 +0000

    Step 1: added a title and a canvas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
-import "./style.css";
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "./style.css";
+document.head.append(link);
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const wrapper = document.createElement("div");
+wrapper.className = "wrapper";
+document.body.append(wrapper);
+
+const title = document.createElement("h1");
+title.textContent = "Sticker Sketchpad";
+title.style.margin = "0";
+title.style.fontSize = "1.25rem";
+wrapper.append(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+wrapper.append(canvas);
+
